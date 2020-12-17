63e389d9474222b4df5e41ca9e18f0c30cb56f80
Kumar, Abhay (q1036048) <abhay.kumar2@quintiles.com>
2020-12-17 03:50:43 -0500

    add api for retrieving similar protocols

--- app/api/endpoints/api.py
+++ app/api/endpoints/api.py
@@ -9,6 +9,7 @@ from app.api.endpoints import saved_search
 from app.api.endpoints import document_process
 from app.api.endpoints import protocol_attributes
 from app.api.endpoints import duplicate_check
+from app.api.endpoints import related_protocols
 
 api_router = APIRouter()
 api_router.include_router(protocol_sponsor.router, prefix="/protocol_sponsor", tags=["Protocol Sponsors"])
@@ -21,4 +22,5 @@ api_router.include_router(recent_search.router, prefix="/recent_search", tags=["
 api_router.include_router(saved_search.router, prefix="/saved_search", tags=["Saved Search"])
 api_router.include_router(document_process.router, prefix="/status", tags=["status"]) 
 api_router.include_router(duplicate_check.router, prefix="/duplicate_check", tags=["Duplicate check"])
+api_router.include_router(related_protocols.router, prefix="/Related_protocols", tags=["Related protocols"])
                       
