dd49a641de069e6b43a87d7889552f48ecbe1e4f
Kumar, Abhay (q1036048) <abhay.kumar2@quintiles.com>
2021-01-28 12:10:42 +0530

    update confifurations for dev

--- app/utilities/config.py
+++ app/utilities/config.py
@@ -2,14 +2,14 @@ from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    PROJECT_NAME: str = "PROTOCOL DIGITALIZATION"
-    API: str = "/api"
-    SQLALCHEMY_DATABASE_URI: str = 'mssql+pyodbc://pd_dev_dbo:$1457abxd@CA2SPDSQL01Q\PDSSQL001D/PD_Dev?driver=ODBC+Driver+17+for+SQL+Server'
-    PROTOCOL_FOLDER : str = '//CA2SPDML06D/protocols'
+    PROJECT_NAME: str
+    API: str 
+    SQLALCHEMY_DATABASE_URI: str 
+    PROTOCOL_FOLDER : str
     # Added for elastic soft delete integration in backend
-    ELASTIC_HOST = 'ca2spdml04q'
-    ELASTIC_PORT = 9200
-    ELASTIC_INDEX = "pd-index-3"
+    ELASTIC_HOST: str
+    ELASTIC_PORT: int
+    ELASTIC_INDEX: str
     
     class Config:
         env_file = ".env"
