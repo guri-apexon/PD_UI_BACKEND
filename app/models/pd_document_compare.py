5551452bd9be1f4a3dae0d8a5a2b05d91772a37e
Mohan, Jiju (q1020640) <jiju.mohan@quintiles.com>
2021-06-03 08:47:14 -0400

    Initial check in for compare download without ut

--- app/models/pd_document_compare.py
+++ app/models/pd_document_compare.py
@@ -5,30 +5,17 @@ from app.db.base_class import Base
 
 class PD_Document_Compare(Base):
 
-    __tablename__ = "pd_document_compare"
+    __tablename__ = "pd_protocol_compare"
 
-    
+
     compareId = Column(String)
     id1 = Column(String, primary_key=True)
-    protocolNumber = Column(String, nullable=True)
-    projectId = Column(String, nullable=True)
-    versionNumber = Column(String, nullable=True)
-    amendmentNumber = Column(String, nullable=True)
-    documentStatus = Column(String, nullable=True)
     id2 = Column(String, primary_key=True)
-    protocolNumber2 = Column(String, nullable=True)
-    projectId2 = Column(String, nullable=True)
-    versionNumber2 = Column(String, nullable=True)
-    amendmentNumber2 = Column(String, nullable=True)
-    documentStatus2 = Column(String, nullable=True)
-    environment = Column(String, nullable=True)
-    sourceSystem = Column(String, nullable=True)
-    userId = Column(String, nullable=True)
-    requestType = Column(String, nullable=True)
-    iqvdata = Column(VARCHAR, nullable=True)
-    baseIqvXmlPath = Column(String, nullable=True)
-    compareIqvXmlPath = Column(String, nullable=True)
-    updatedIqvXmlPath = Column(String, nullable=True)
-    similarityScore = Column(Integer, nullable=True)
-    swap = Column(Boolean, nullable=True)
-    
+    protocolNumber = Column(String(45))
+    compareIqvXmlPath = Column(String(1000))
+    compareCSVPath = Column(String(1000))
+    compareJSONPath =Column(String(1000))
+    numChangesTotal = Column(Integer())
+    swap = Column(Boolean(), default=False)
+    createdDate = Column(DateTime)
+    updatedDate = Column(DateTime)
