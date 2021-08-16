65d9163520e0d53d333f47049b7eefa419789178
Raju, Shankar (q1034911) <shankar.raju@quintiles.com>
2021-08-16 04:08:56 -0400

    pd-release/1.3 changes to develop

--- app/utilities/elastic_utilities.py
+++ app/utilities/elastic_utilities.py
@@ -1,8 +1,8 @@
 import logging
-
-from elasticsearch import Elasticsearch
+from datetime import datetime
 
 from app.utilities.config import settings
+from elasticsearch import Elasticsearch, helpers
 
 logger = logging.getLogger(settings.LOGGER_NAME)
 
@@ -30,6 +30,35 @@ def update_elastic(update_json, aidocid):
 
     es.close()
     return res
+
+def update_bulk_elastic(es_update_dict: dict):
+    current_timestamp = datetime.utcnow()
+    current_utc_num_format = current_timestamp.strftime("%Y%m%d%H%M%S")
+    return_code = True
+    success_num = -1
+    failures = []
+    try:
+        es = Elasticsearch([{'host': settings.ELASTIC_HOST, 'port': settings.ELASTIC_PORT}])
+        es_sync =   [
+                        {
+                        '_id': aidoc_id, 
+                        '_index': settings.ELASTIC_INDEX,
+                        "_source": {'doc': dict({'TimeUpdated': current_utc_num_format}, **update_fields)}, 
+                        '_op_type': 'update'
+                        } 
+                        for aidoc_id, update_fields in es_update_dict.items()
+                    ]
+        logger.info(f"Bulk update attempt for {len(es_sync)} documents")
+        success_num, failures = helpers.bulk(client=es, actions=es_sync, request_timeout=600, raise_on_error=False)
+        logger.info(f"Successfully updated docs #: {success_num}; Failure doc #: {len(failures)}; Details: {failures}")
+    except Exception as exc:
+        logger.exception(f"update_bulk_elastic: {exc}")
+        return_code = False
+    finally:
+        es.close()
+    
+    return return_code, success_num, failures
+
 
 def get_elastic_doc_by_id(aidocid):
     try:
