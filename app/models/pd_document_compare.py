5fd44c0ba5824ffedcbbf62d4493c9a541d832cb
Abhay Kumar <abhay.kumar2@quintiles.com>
2021-09-29 11:08:37 +0000

    Feature/1.4 update compare api to respond as per redaction profile

--- app/models/pd_document_compare.py
+++ app/models/pd_document_compare.py
@@ -8,13 +8,19 @@ class PD_Document_Compare(Base):
     __tablename__ = "pd_protocol_compare"
 
 
-    compareId = Column(String)
-    id1 = Column(String, primary_key=True)
-    id2 = Column(String, primary_key=True)
+    compareId = Column(String, primary_key=True)
+    id1 = Column(String)
+    id2 = Column(String)
     protocolNumber = Column(String(45))
     compareIqvXmlPath = Column(String(1000))
     compareCSVPath = Column(String(1000))
     compareJSONPath =Column(String(1000))
+    compareCSVPathNormSOA = Column(String(1000))
+    compareJSONPathNormSOA = Column(String(1000))
+    redactProfile = Column(String(100))
+    feedbackRun = Column(Integer())
+    compareExcelPathNormSOA = Column(String(1000))
+    compareHTMLPathNormSOA = Column(String(1000))
     numChangesTotal = Column(Integer())
     swap = Column(Boolean(), default=False)
     createdDate = Column(DateTime)
