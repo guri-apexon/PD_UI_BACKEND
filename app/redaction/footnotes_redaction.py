5cd2c57708e59ac4928a4513ab20c819fadc2b68
q1061485 <abhilash.basutkar@quintiles.com>
2021-10-06 13:27:16 +0530

    PD-2545 Table Redaction Sub task PD-2577

--- app/redaction/footnotes_redaction.py
+++ app/redaction/footnotes_redaction.py
@@ -1,38 +1,38 @@
-# import logging
-# from app import config
-# from app.utilities.config import settings
-#
-# logger = logging.getLogger(settings.PROJECT_NAME)
-#
-#
-# class RedactFootNotes:
-#     def __init__(self, attachments: list, redacted_placeholder: str, redact_flag: bool):
-#         self.attachments = attachments
-#         self.redacted_placeholder = redacted_placeholder
-#         self.redact_flag = redact_flag
-#
-#     def redact_footnotes(self, content: str, entities: dict) -> str:
-#         if content:
-#             for entity in sorted(entities, key=lambda x: (x[config.FOOTNOTES_START_INDEX],
-#                                                           x[config.FOOTNOTES_END_INDEX]), reverse=True):
-#                 content = content[0:entity[config.FOOTNOTES_START_INDEX]] + \
-#                           self.redacted_placeholder + \
-#                           content[entity[config.FOOTNOTES_END_INDEX]+1:]
-#         return content
-#
-#     def redact_footnotes_pipeline(self):
-#         redacted_json = {}
-#         try:
-#             if self.attachments:
-#                 for idx, attachment in enumerate(self.attachments):
-#                     text = attachment[config.FOOTNOTES_TEXT]
-#                     entities = attachment[config.FOOTNOTES_ENTITIES]
-#                     if self.redact_flag:
-#                         redacted_text = self.redact_footnotes(text, entities)
-#                     else:
-#                         redacted_text = text
-#                     foot_note_key = f"{config.FOOTNOTE_STR}_{idx}"
-#                     redacted_json[foot_note_key] = " ".join([attachment[config.FOOTNOTES_KEY], redacted_text]).strip()
-#         except AttributeError as exc:
-#             logger.error(f"AttributeError raised: {str(exc)}")
-#         return redacted_json
+import logging
+from app import config
+from app.utilities.config import settings
+
+logger = logging.getLogger(settings.PROJECT_NAME)
+
+
+class RedactFootNotes:
+    def __init__(self, attachments: list, redacted_placeholder: str, redact_flag: bool):
+        self.attachments = attachments
+        self.redacted_placeholder = redacted_placeholder
+        self.redact_flag = redact_flag
+
+    def redact_footnotes(self, content: str, entities: dict) -> str:
+        if content:
+            for entity in sorted(entities, key=lambda x: (x[config.FOOTNOTES_START_INDEX],
+                                                          x[config.FOOTNOTES_END_INDEX]), reverse=True):
+                content = content[0:entity[config.FOOTNOTES_START_INDEX]] + \
+                          self.redacted_placeholder + \
+                          content[entity[config.FOOTNOTES_END_INDEX]+1:]
+        return content
+
+    def redact_footnotes_pipeline(self):
+        redacted_json = {}
+        try:
+            if self.attachments:
+                for idx, attachment in enumerate(self.attachments):
+                    text = attachment[config.FOOTNOTES_TEXT]
+                    entities = attachment[config.FOOTNOTES_ENTITIES]
+                    if self.redact_flag:
+                        redacted_text = self.redact_footnotes(text, entities)
+                    else:
+                        redacted_text = text
+                    foot_note_key = f"{config.FOOTNOTE_STR}_{idx}"
+                    redacted_json[foot_note_key] = " ".join([attachment[config.FOOTNOTES_KEY], redacted_text]).strip()
+        except AttributeError as exc:
+            logger.error(f"AttributeError raised: {str(exc)}")
+        return redacted_json
