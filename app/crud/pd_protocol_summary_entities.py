13c905ea7ead114b57f6471b031282e4bd2be2a4
Abhinav Daksha <abhinav.daksha@quintiles.com>
2021-12-13 06:56:15 +0000

    Feature/additional attribute entities handling

--- app/crud/pd_protocol_summary_entities.py
+++ app/crud/pd_protocol_summary_entities.py
@@ -1,3 +1,4 @@
+import json
 from typing import Any, Dict, Optional, Union
 from sqlalchemy.orm import Session
 from app.crud.base import CRUDBase
@@ -9,12 +10,20 @@ class CRUDProtocols(CRUDBase[PDProtocolSummaryEntities, ProtocolSummaryEntitiesC
     @staticmethod
     def get_protocol_summary_entities(db: Session, aidocId: Any) -> Optional[PDProtocolSummaryEntities]:
         """Retrieves a record based on primary key or id"""
-        return db.query(PDProtocolSummaryEntities.aidocId,
-                        PDProtocolSummaryEntities.source,
-                        PDProtocolSummaryEntities.runId,
-                        PDProtocolSummaryEntities.iqvdataSummaryEntities,
-                        ).filter(PDProtocolSummaryEntities.aidocId == aidocId,
-                                 PDProtocolSummaryEntities.isActive == True).first()
+        summary_entities = {}
+        summary_db_records = db.query(PDProtocolSummaryEntities.aidocId,
+                                      PDProtocolSummaryEntities.source,
+                                      PDProtocolSummaryEntities.runId,
+                                      PDProtocolSummaryEntities.iqvdataSummaryEntities,
+                                      ).filter(PDProtocolSummaryEntities.aidocId == aidocId,
+                                               PDProtocolSummaryEntities.isActive == True).first()
+
+        if summary_db_records and summary_db_records.iqvdataSummaryEntities:
+            try:
+                summary_entities = json.loads(json.loads(summary_db_records.iqvdataSummaryEntities))
+            except Exception:
+                ...
+        return summary_entities
 
     def create(self, db: Session, *, obj_in: ProtocolSummaryEntitiesCreate) -> PDProtocolSummaryEntities:
         ...
