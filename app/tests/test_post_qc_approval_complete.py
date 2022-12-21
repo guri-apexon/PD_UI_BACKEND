4aa4edf23d65eef30784bce6f8cf232851ab2bef
P, Sreenuvasulu <sreenuvasulu.p@iqvia.com>
2022-12-21 14:03:54 +0000

    fix: update test cases

--- app/tests/test_post_qc_approval_complete.py
+++ app/tests/test_post_qc_approval_complete.py
@@ -14,5 +14,5 @@ def test_management_health():
 
 def test_post_qc_complete_management_url():
     response = requests.post(settings.MANAGEMENT_SERVICE_URL + "pd_qc_check_update",
-                             json={'aidoc_id': 'a89de6a0-fc10-4964-9364-fa20962d44ef', 'qcApprovedBy': 'ut_id4', 'parent_path': '//quintiles.net/enterprise/Services/protdigtest/pilot_iqvxml/a89de6a0-fc10-4964-9364-fa20962d44ef'}, headers = settings.MGMT_CRED_HEADERS)
+                             json={'aidoc_id': '62166a73-bb80-441e-8ef7-4c09cce5a5d9', 'qcApprovedBy': 'ut_id4', 'parent_path': '//quintiles.net/enterprise/Services/protdigtest/pilot_iqvxml/62166a73-bb80-441e-8ef7-4c09cce5a5d9'}, headers = settings.MGMT_CRED_HEADERS)
     assert response.status_code == 200
