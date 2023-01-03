bfa040fc8958c137c63262a3dea8b628531e2e30
1151186 <mouneshwara.sugappa@iqvia.com>
2023-01-03 11:31:31 +0530

    added test cases for get_section_data

--- app/tests/test_cpt_data.py
+++ app/tests/test_cpt_data.py
@@ -10,10 +10,17 @@ logger = logging.getLogger("unit-test")
 
 
 @pytest.mark.parametrize("doc_id, link_level, toc, status_code, comments", [
-    ("5c784c05-fbd3-4786-b0e4-3afa0d1c61ac", "1", "1", 200, "doc older than legacy_upload_date for secondary users"),
-    ("1698be28-1cf3-466e-8f56-5fc920029057", "1", "1", 200, "doc newer than legacy_upload_date"),
-    ("1698be28-1cf3-466e-8f56-5fc9200290571", "1", "1", 206, "doc newer than legacy_upload_date"),
+    ("5c784c05-fbd3-4786-b0e4-3afa0d1c61ac", "1", "1", 200,
+     "doc older than legacy_upload_date for secondary users"),
+    ("1698be28-1cf3-466e-8f56-5fc920029057", "1",
+     "1", 200, "doc newer than legacy_upload_date"),
+    ("1698be28-1cf3-466e-8f56-5fc9200290571", "1",
+     "1", 206, "doc newer than legacy_upload_date"),
     ("1698be28-1cf3-466e-8f56-5fc920029057", "1", "2", 206, "doc newer than legacy_upload_date")])
-def test_old_processed_doc(new_token_on_headers, doc_id, link_level, toc, status_code, comments):
-    get_cpt = client.get("/api/cpt_data/", params={"aidoc_id": doc_id, "linklevel": link_level, "toc": toc }, headers = new_token_on_headers)
+def test_document_header(new_token_on_headers, doc_id, link_level, toc, status_code, comments):
+    """
+        Tests CPT Sections/Headers  list for a particular document with doc_id, link_level, toc, and comments.
+    """
+    get_cpt = client.get("/api/cpt_data/", params={
+                         "aidoc_id": doc_id, "linklevel": link_level, "toc": toc}, headers=new_token_on_headers)
     assert get_cpt.status_code == status_code
--- app/tests/test_cpt_section_data.py
+++ app/tests/test_cpt_section_data.py
@@ -1,9 +1,7 @@
 import logging
-
 import pytest
 from app.db.session import SessionLocal
 from app.main import app
-
 from fastapi.testclient import TestClient
 
 client = TestClient(app)
@@ -12,10 +10,19 @@ logger = logging.getLogger("unit-test")
 
 
 @pytest.mark.parametrize("doc_id, link_level, link_id, user_id, protocol, user_type, status_code, comments", [
-    ("5c784c05-fbd3-4786-b0e4-3afa0d1c61ac", "1", "","1034911", "SSRUT_GEN_001", "normal", 401, "doc older than legacy_upload_date for secondary users"),
-    ("1698be28-1cf3-466e-8f56-5fc920029057","1", "", "1036048", "FEED_TEST4", "normal", 200, "doc newer than legacy_upload_date")
+    ("5c784c05-fbd3-4786-b0e4-3afa0d1c61ac", "1", "", "1034911", "SSRUT_GEN_001",
+     "normal", 200, "doc older than legacy_upload_date for secondary users"),
+    ("1698be28-1cf3-466e-8f56-5fc920029057", "1", "", "1036048",
+     "FEED_TEST4", "normal", 200, "doc newer than legacy_upload_date"),
+    ("1698be28-1cf3-466e-8f56-5fc920029056", "1", "", "1036048",
+     "FEED_TEST4", "normal", 206, "doc newer than legacy_upload_date"),
+    ("21552918-f506-43d8-8879-4fe532631ba7", "", "8f8e70a7-cb76-4257-b595-80a2564a8aa2", "Dig2_Batch_Tester",
+     "BI.Obesity.91af1307-5fc5-40cd-9671-b82771a42b2f", "normal", 200, "doc newer than legacy_upload_date")
 ])
-def test_old_processed_doc(new_token_on_headers, user_id, protocol, doc_id, user_type, status_code, comments,link_level,link_id):
-
-    get_cpt_section_data = client.get("/api/cpt_section_data/", params={"aidoc_id": doc_id, "linklevel": link_level, "linlid": link_id , "userId": user_id, "protocol": protocol, "user": user_type}, headers = new_token_on_headers)
+def test_document_object(new_token_on_headers, user_id, protocol, doc_id, user_type, status_code, comments, link_level, link_id):
+    """
+        Tests document Section/Header data for document with doc_id, protocol, user_id, link_level and link_id.
+    """
+    get_cpt_section_data = client.get("/api/cpt_data/get_section_data/", params={
+                                      "aidoc_id": doc_id, "linklevel": link_level, "linlid": link_id, "userId": user_id, "protocol": protocol}, headers=new_token_on_headers)
     assert get_cpt_section_data.status_code == status_code
