86d6843647896a788ef0c9a19be2ee402b2a8c09
1151186 <mouneshwara.sugappa@iqvia.com>
2023-01-11 16:31:18 +0530

    test cases fixed

--- app/tests/test_cpt_section_data.py
+++ app/tests/test_cpt_section_data.py
@@ -13,7 +13,7 @@ logger = logging.getLogger("unit-test")
     ("5c784c05-fbd3-4786-b0e4-3afa0d1c61ac", "1", "", "1034911",
      "SSRUT_GEN_001", 200, "doc id and without link id"),
     ("1698be28-1cf3-466e-8f56-5fc920029056", "1", "", "1036048",
-     "FEED_TEST4", 206, "doc id changes"),
+     "FEED_TEST4", 404, "doc id changes"),
     ("21552918-f506-43d8-8879-4fe532631ba7", "", "8f8e70a7-cb76-4257-b595-80a2564a8aa2", "Dig2_Batch_Tester",
      "BI.Obesity.91af1307-5fc5-40cd-9671-b82771a42b2f", 200, "doc id and link id present")
 ])
