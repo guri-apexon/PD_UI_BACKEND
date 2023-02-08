55719921384eb10b2635c02c5417c558c6b00c16
U1149484 <mukeshkumar.kumawat@iqvia.com>
2023-02-08 10:25:49 +0530

    removed build folder

--- build/lib/app/api/endpoints/health_check.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-from fastapi.responses import PlainTextResponse
-
-router = APIRouter()
-
-
-@router.get("/", response_class=PlainTextResponse)
-def healthcheck():
-    return "F5-UP"
--- build/lib/app/tests/conftest.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from app.tests import utils as test_utils
-
-@pytest.fixture(scope="module", autouse=True)
-def new_token_on_headers():
-    return test_utils.get_token()
