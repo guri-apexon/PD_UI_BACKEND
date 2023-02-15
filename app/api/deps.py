17c801e15b7db047f4e6cfbeca095a3b7482caaf
1151186 <mouneshwara.sugappa@iqvia.com>
2023-02-15 14:20:35 +0530

    altered DB connections

--- app/api/deps.py
+++ app/api/deps.py
@@ -9,13 +9,3 @@ def get_db() -> Generator:
         yield db
     finally:
         db.close()
-
-
-# connection for postgresql
-def get_psqldb() -> Generator:
-    try:
-        psdb = SessionPSQL()
-        yield psdb
-    finally:
-        psdb.close()
-
