c201f576eae3de4fe48acf44210840f5f97bd199
q1061485 <abhilash.basutkar@quintiles.com>
2023-02-16 13:15:58 +0530

    DB Table name fix

--- app/models/pd_protocol_sponsor.py
+++ app/models/pd_protocol_sponsor.py
@@ -5,7 +5,7 @@ from app.db.base_class import Base
 
 class PD_Protocol_Sponsor(Base):
 
-    __tablename__ = "PD_Protocol_Sponsor"
+    __tablename__ = "pd_protocol_sponsor"
 
     sponsorId = Column(Integer, primary_key=True, index=True)
     sponsorName = Column(String, nullable=True)
