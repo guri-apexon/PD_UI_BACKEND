41e5127763be7450e19329f4607ca581cde32d57
1151186 <mouneshwara.sugappa@iqvia.com>
2023-02-24 12:42:52 +0530

    worked on config api MR comments

--- app/crud/pd_document_config_terms.py
+++ app/crud/pd_document_config_terms.py
@@ -1,5 +1,6 @@
 from app.utilities.config import settings
 from sqlalchemy.orm import Session
+from typing import Tuple
 import logging
 from app.models.pd_iqvvisitrecord_db import IqvvisitrecordDb
 from app.models.pd_nlp_entity_db import NlpEntityDb
@@ -13,7 +14,7 @@ logger = logging.getLogger(settings.LOGGER_NAME)
 
 
 def get_document_terms_data(db: Session, aidoc_id: str,
-                            link_id: str, config_variables: str, section_text: str) -> list:
+                            link_id: str, config_variables: str, link_dict: dict) -> list:
     """
     Collect terms values based on provided configuration values for the
     document section
@@ -31,19 +32,6 @@ def get_document_terms_data(db: Session, aidoc_id: str,
         :param properties -- optional
     :returns: list of configurable terms values
     """
-    link_dict = {}
-    if section_text:
-        try:
-            get_link_id = db.query(IqvdocumentlinkDb).filter(
-                IqvdocumentlinkDb.doc_id == aidoc_id, IqvdocumentlinkDb.LinkText == section_text).one()
-            link_id = get_link_id.link_id
-            link_dict.update({"link_id":get_link_id.link_id,"link_id_level2":get_link_id.link_id_level2,"link_id_level3":get_link_id.link_id_level3
-                                ,"link_id_level4":get_link_id.link_id_level4,"link_id_level5":get_link_id.link_id_level5,"link_id_level6":get_link_id.link_id_level6})
-            link_dict = {k:v for k,v in link_dict.items() if v}
-        except Exception as e:
-            logger.exception(
-                f"Exception occured during getting link id {section_text}, {str(e)}")
-            link_id = ""
 
     terms_values = {}
 
@@ -151,3 +139,32 @@ def get_document_terms_data(db: Session, aidoc_id: str,
         logger.info(f"redaction attributes results {redaction_att_values}")
 
     return [terms_values]
+
+
+def link_id_link_level_based_on_section_text(psdb: Session, aidoc_id: str, section_text: str, link_id: str, link_level: str = "") -> Tuple[int, str, dict]:
+    """
+    :param db: db session
+    :param aidoc_id: document id
+    :param link_level: level of headers in toc
+    :param link_id: section id    
+    :returns link_id and link_level based on section text 
+        and link_dict is link_levels and link ids as key and values
+    """
+    link_dict = {}
+    if section_text:
+        try:
+            get_link_id = psdb.query(IqvdocumentlinkDb).filter(
+                IqvdocumentlinkDb.doc_id == aidoc_id, IqvdocumentlinkDb.LinkText == section_text).one()
+            link_dict.update({"link_id":get_link_id.link_id,"link_id_level2":get_link_id.link_id_level2,"link_id_level3":get_link_id.link_id_level3
+                                ,"link_id_level4":get_link_id.link_id_level4,"link_id_level5":get_link_id.link_id_level5,"link_id_level6":get_link_id.link_id_level6})
+            link_dict = {k:v for k,v in link_dict.items() if v}
+            link_level_identify = list(link_dict.keys())
+            if len(link_level_identify) > 1:
+                link_level = int(link_level_identify[-1].strip("link_id_level"))
+                link_id = link_dict[link_level_identify[-1]]
+            return link_id, link_level, link_dict
+        except Exception as e:
+            logger.exception(
+                f"Exception occured during getting link id {section_text}, {str(e)}")
+
+    return link_id, link_level, link_dict
