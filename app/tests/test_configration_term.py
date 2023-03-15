f9e87a2ca7b1b7b81f35d2ee0cea7ac3d8a92917
Sugappa, Mouneshwara EX1 <mouneshwara.sugappa@iqvia.com>
2023-03-15 08:57:25 +0000

    Feature/config api fix

--- app/tests/test_configration_term.py
+++ app/tests/test_configration_term.py
@@ -19,11 +19,7 @@ def test_configration_variables(new_token_on_headers, user_id, protocol, doc_id,
     get_config_variables = client.get("/api/cpt_data/get_section_data_configurable_parameter/", params={
                                       "aidoc_id": doc_id, "linklevel": link_level, "link_id": link_id, "userId": user_id, "protocol": protocol, "section_text": section_text, "config_variables": config_variables}, headers=new_token_on_headers)
     
-    
-    if status_code == status.HTTP_404_NOT_FOUND:
-        assert get_config_variables.json()[0]['status_code'] == status_code
-        
-    elif comments == "NO_TIME_POINT_DATA":
+    if comments == "NO_TIME_POINT_DATA":
         assert 'time_points' not in get_config_variables.json()[1][0].keys()
     
     elif comments == "NO_REFERENCE_DATA":
