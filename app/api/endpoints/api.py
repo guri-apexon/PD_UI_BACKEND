cf349d62b64327c868685766e2624560b692be57
Sugappa, Mouneshwara EX1 <mouneshwara.sugappa@iqvia.com>
2023-03-15 13:44:47 +0000

    Feature/pd 4402 api

--- app/api/endpoints/api.py
+++ app/api/endpoints/api.py
@@ -33,6 +33,7 @@ from app.api.endpoints import pd_roles_GetAllRoles_CreateNewRoles
 from app.api.endpoints import ldap_user_details
 from app.api.endpoints import cpt_data
 from app.api.endpoints import qc_ingest
+from app.api.endpoints import user_alert_setting
 from app.api.endpoints import pd_elastic_ingest
 
 api_router = APIRouter()
@@ -70,4 +71,5 @@ api_router.include_router(pd_roles_GetAllRoles_CreateNewRoles.router, prefix="/r
 api_router.include_router(ldap_user_details.router, prefix="/ldap_user_details", tags=["Retrieve User Details From LDAP"])
 api_router.include_router(cpt_data.router, prefix="/cpt_data", tags=["Get CPT data for document"])
 api_router.include_router(qc_ingest.router, prefix="/qc_ingest", tags=["QC Ingest"])
+api_router.include_router(user_alert_setting.router, prefix="/user_alert_setting", tags=["User Alert Setting to get notification"])
 api_router.include_router(pd_elastic_ingest.router, prefix="/elastic_ingest", tags=["Elastic Ingest"])
