e3c1e0e47958caa7a72b791e3cb1f2975f37392c
Kumawat, Mukesh Kumar EX1 <mukeshkumar.kumawat@iqvia.com>
2023-03-22 14:31:33 +0000

    Feature auditlog

--- app/qc_ingest/model/__init__.py
+++ app/qc_ingest/model/__init__.py
@@ -3,5 +3,6 @@ from .documenttables_db import DocumenttablesDb
 from .iqvdocumentimagebinary_db import IqvdocumentimagebinaryDb
 from .iqvdocument_link_db import IqvdocumentlinkDb
 from .iqvpage_roi_db import IqvpageroiDb
+from .iqvsectionlock_db import IqvsectionlockDb
 from .__base__ import SchemaBase
 
