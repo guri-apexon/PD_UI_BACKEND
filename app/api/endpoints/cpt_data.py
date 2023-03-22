a2f122fba0c1aa45cbc7a52e6bb7092bfab218dc
urvesh.patel <urvesh.patel@iqvia.com>
2023-03-22 10:36:00 +0530

    PD-4003: Function added to utilities and updated changes

--- app/api/endpoints/cpt_data.py
+++ app/api/endpoints/cpt_data.py
@@ -10,6 +10,7 @@ from app.utilities.config import settings
 from app.api.endpoints import auth
 from app.utilities.redaction.protocol_view_redaction import \
     ProtocolViewRedaction
+from crud.utilities import get_preffered_data
 from fastapi.responses import JSONResponse
 from fastapi import status
 import logging
@@ -75,38 +76,6 @@ async def get_enriched_data(
         }
         clinical_data.append(clinical_values)
     return clinical_data
-
-
-async def get_preffered_data(
-        db: Session = Depends(deps.get_db),
-        doc_id: str = "",
-        link_id: str = "",
-        _: str = Depends(auth.validate_user_token)
-) -> Any:
-    """
-    Get preffered terms values for the enriched text as per doc and section id
-    :param db: database session
-    :param doc_id: document id
-    :param link_id: link id of document as section id
-    :param _: To validate API token
-    :returns: To collect all the preffered terms values for the enriched text
-    from all over the section
-    """
-    config_variables = {"preferred_terms" : True}
-    preffered_document_data = crud.get_document_terms_data(db, doc_id,
-                                            link_id, config_variables, {})   
-    
-    preffered_data = []
-    for entity in preffered_document_data[0].get("preferred_terms"):
-        preffered_values = {
-            'id': entity["id"],
-            'parent_id': entity["parent_id"],
-            'preferred_term': entity["preferred_term"],
-            'text': entity["text"] 
-        }
-        preffered_data.append(preffered_values)
-
-    return preffered_data
 
 
 @router.get("/get_section_data")
