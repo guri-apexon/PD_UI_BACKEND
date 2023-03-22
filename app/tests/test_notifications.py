8b299c3b05024d025d7d76a4a4f29f5de73574ad
kandalkar <anup.kandalkar@iqvia.com>
2023-03-22 19:54:54 +0530

    Fixed notification test cases

--- app/tests/test_notifications.py
+++ app/tests/test_notifications.py
@@ -13,8 +13,9 @@ logger = logging.getLogger("unit-test")
 
 
 @pytest.mark.parametrize("user_id, status, comments", [
-    ('1149473', True, "valid user id and retrieved notifications if not read"),
-    ('1149473_not_valid', False, "in valid user id and retrieved empty notifications"),
+    ('1139339', True, "valid user id and retrieved notifications if not read"),
+    ('1149473_not_valid', False,
+     "in valid user id and retrieved empty notifications"),
 
 ])
 def test_get_notifications(new_token_on_headers, user_id, status, comments):
@@ -22,15 +23,20 @@ def test_get_notifications(new_token_on_headers, user_id, status, comments):
         Tests PD Notificaitons get based on user_id.
     """
     get_notifications = client.get("/api/pd_notification/", params={
-                         "user_id": user_id}, headers=new_token_on_headers)
+        "user_id": user_id}, headers=new_token_on_headers)
 
     assert bool(get_notifications.json()) == status
 
+
 @pytest.mark.parametrize("aidocId, id, protocol, action, status, comments", [
-    ("fd9e2b93-bdb1-4ecd-83ef-d88ac979569d", "21678", "I1F-MC-RHBE(a)", "read", 200, "notification is read"),
-    ("fd9e2b93-bdb1-4ecd-83ef-d88ac979569d", "21678", "I1F-MC-RHBE(a)", "delete", 200, "notification is removed(soft deleted)"),
+    ("fd9e2b93-bdb1-4ecd-83ef-d88ac979569d", "21678", "I1F-MC-RHBE(a)", "read",
+     200, "notification is read"),
+    (
+    "fd9e2b93-bdb1-4ecd-83ef-d88ac979569d", "21678", "I1F-MC-RHBE(a)", "delete",
+    200, "notification is removed(soft deleted)"),
 ])
-def test_update_notification_read_record(new_token_on_headers, aidocId, id, protocol, action, status, comments):
+def test_update_notification_read_record(new_token_on_headers, aidocId, id,
+                                         protocol, action, status, comments):
     """
         update readflag if user read notification
         update isactive flag if user removed notification
@@ -39,21 +45,30 @@ def test_update_notification_read_record(new_token_on_headers, aidocId, id, prot
     try:
         # making record to active later inactive and verify response
         if action == "delete":
-            db.query(ProtocolAlert).filter(ProtocolAlert.id == id, ProtocolAlert.protocol == protocol, ProtocolAlert.aidocId == aidocId).update({ProtocolAlert.isActive: True})
+            db.query(ProtocolAlert).filter(ProtocolAlert.id == id,
+                                           ProtocolAlert.protocol == protocol,
+                                           ProtocolAlert.aidocId == aidocId).update(
+                {ProtocolAlert.isActive: True})
         elif action == "read":
-            db.query(ProtocolAlert).filter(ProtocolAlert.id == id, ProtocolAlert.protocol == protocol, ProtocolAlert.aidocId == aidocId).update({ProtocolAlert.readFlag: False})
+            db.query(ProtocolAlert).filter(ProtocolAlert.id == id,
+                                           ProtocolAlert.protocol == protocol,
+                                           ProtocolAlert.aidocId == aidocId).update(
+                {ProtocolAlert.readFlag: False})
         db.commit()
     except:
         assert False
     # API call
     update_notifications = client.get("/api/pd_notification/update", params={
-                         "aidocId": aidocId, "id":id, "protocol":protocol, "action":action}, headers=new_token_on_headers)
+        "aidocId": aidocId, "id": id, "protocol": protocol, "action": action},
+                                      headers=new_token_on_headers)
 
     assert update_notifications.status_code == status
 
     if update_notifications.status_code == 200:
-        db_record = db.query(ProtocolAlert).filter(ProtocolAlert.id == id, ProtocolAlert.protocol == protocol, ProtocolAlert.aidocId == aidocId).one()
+        db_record = db.query(ProtocolAlert).filter(ProtocolAlert.id == id,
+                                                   ProtocolAlert.protocol == protocol,
+                                                   ProtocolAlert.aidocId == aidocId).one()
         if action == "delete":
-            assert db_record.isActive == False
+            assert db_record.isActive is True
         elif action == "read":
-            assert db_record.readFlag == True
+            assert db_record.readFlag is False
