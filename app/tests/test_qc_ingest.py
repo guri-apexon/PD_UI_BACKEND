39a157c2744f9a5d4afcf78e87587cfa3f8b6377
Kumawat, Mukesh Kumar EX1 <mukeshkumar.kumawat@iqvia.com>
2023-03-31 07:17:28 +0000

    Feature table payload format change

--- app/tests/test_qc_ingest.py
+++ app/tests/test_qc_ingest.py
@@ -97,51 +97,6 @@ def get_payload(file_name):
         return payload
 
 
-def insert_row(uuid, new_token_on_headers):
-    qc_ingest_test_data = r"./app/tests/data/qc_ingest_table_data_add_row.json"
-    payload = get_payload(qc_ingest_test_data)
-    payload[0]['line_id'] = uuid
-    response = client.post(
-        TEST_END_POINT, json=payload, headers=new_token_on_headers)
-    assert response.status_code == 200
-
-
-def insert_col(uuid, new_token_on_headers):
-    qc_ingest_test_data = r"./app/tests/data/qc_ingest_table_data_add column.json"
-    payload = get_payload(qc_ingest_test_data)
-    payload[0]['line_id'] = uuid
-    response = client.post(
-        TEST_END_POINT, json=payload, headers=new_token_on_headers)
-    assert response.status_code == 200
-
-
-def delete_row(uuid, new_token_on_headers):
-    qc_ingest_test_data = r"./app/tests/data/qc_ingest_table_delete_row.json"
-    payload = get_payload(qc_ingest_test_data)
-    payload[0]['line_id'] = uuid
-    response = client.post(
-        TEST_END_POINT, json=payload, headers=new_token_on_headers)
-    assert response.status_code == 200
-
-
-def delete_column(uuid, new_token_on_headers):
-    qc_ingest_test_data = r"./app/tests/data/qc_ingest_table_column_delete.json"
-    payload = get_payload(qc_ingest_test_data)
-    payload[0]['line_id'] = uuid
-    response = client.post(
-        TEST_END_POINT, json=payload, headers=new_token_on_headers)
-    assert response.status_code == 200
-
-
-def modify_data(uuid, new_token_on_headers):
-    qc_ingest_test_data = r"./app/tests/data/qc_ingest_table_data_modify.json"
-    payload = get_payload(qc_ingest_test_data)
-    payload[0]['line_id'] = uuid
-    response = client.post(
-        TEST_END_POINT, json=payload, headers=new_token_on_headers)
-    assert response.status_code == 200
-
-
 def create_table(qc_ingest_test_data, new_token_on_headers):
     with open(qc_ingest_test_data, 'r') as f:
         data = f.read()
@@ -161,52 +116,6 @@ def delete_table(uuid, new_token_on_headers):
         TEST_END_POINT, json=payload, headers=new_token_on_headers)
     assert response.status_code == 200
 
-
-@pytest.mark.parametrize("qc_ingest_test_data", [
-    (r"./app/tests/data/qc_ingest_table_data_create.json")
-])
-def test_document_table_curd(new_token_on_headers, qc_ingest_test_data):
-    """
-    Operations read data and does clean up
-    For Verification :
-    SELECT * FROM public.iqvdocumentlink_db WHERE "id"='34000496-bx4r-1pef-8aab-10505xab64ft'
-    SELECT "Value" FROM public.documentparagraphs_db WHERE "id"='34000496-bx4r-1pef-8aab-10505xab64ft'
-    SELECT * FROM public.documentpartslist_db WHERE "id"='34000496-bx4r-1pef-8aab-10505xab64ft'
-    """
-
-    uuid = create_table(qc_ingest_test_data, new_token_on_headers)
-    modify_data(uuid, new_token_on_headers)
-    data = get_table_data(uuid)
-    assert data[0][0]['val'] == "10 mod"
-    assert data[0][1]['val'] == '11 mod'
-    assert data[1][0]['val'] == '15 mod'
-    # add row ...
-    insert_row(uuid, new_token_on_headers)
-    data = get_table_data(uuid)
-    assert data[2][0]['val'] == "X"
-    assert data[2][1]['val'] == 'Y'
-    delete_row(uuid, new_token_on_headers)
-    data = get_table_data(uuid)
-    assert data[2][0]['val'] == "14"
-    assert data[2][1]['val'] == '15'
-
-    insert_col(uuid, new_token_on_headers)
-    data = get_table_data(uuid)
-    assert data[0][1]['val'] == "AA"
-    assert data[1][1]['val'] == 'BB'
-    assert data[2][1]['val'] == 'CC'
-    assert data[3][1]['val'] == 'DD'
-
-    delete_column(uuid, new_token_on_headers)
-    data = get_table_data(uuid)
-    assert data[0][1]['val'] == "11 mod"
-    assert data[1][1]['val'] == '13'
-    assert data[2][1]['val'] == '15'
-    assert data[3][1]['val'] == '17'
-
-    delete_table(uuid, new_token_on_headers)
-    data = get_table_data(uuid)
-    assert data == {}
 
 def modify_data_all(uuid, new_token_on_headers):
     qc_ingest_test_data = r"./app/tests/data/qc_ingest_table_data_modify_table.json"
@@ -232,14 +141,37 @@ def test_document_table_curd_all_modifiction(new_token_on_headers, qc_ingest_tes
     modify_data_all(uuid, new_token_on_headers)
     data = get_table_data(uuid)
 
-    assert data[0][0]['val'] == "10 mod"
-    assert data[1][0]['val'] == '15 mod'
-    assert data[2][0]['val'] == '14'
-    assert data[3][0]['val'] == '16'
-    assert data[0][1]['val'] == "11 mod"
-    assert data[1][1]['val'] == '13'
-    assert data[2][1]['val'] == '15'
-    assert data[3][1]['val'] == '17'
+    assert data[0][0]['val'] == "0 new"
+    assert data[0][1]['val'] == '1 new'
+    assert data[0][2]['val'] == '2 new'
+    assert data[0][3]['val'] == '3 new'
+    assert data[0][4]['val'] == "4 new"
+    assert data[0][5]['val'] == '5 new'
+    assert data[1][0]['val'] == "1"
+    assert data[1][1]['val'] == '2'
+    assert data[1][2]['val'] == '3 mod'
+    assert data[1][3]['val'] == '4'
+    assert data[1][4]['val'] == "5 mod"
+    assert data[1][5]['val'] == '5'
+    assert data[2][0]['val'] == "6 mod"
+    assert data[2][1]['val'] == '7 mod'
+    assert data[2][2]['val'] == '8 new'
+    assert data[2][3]['val'] == '9'
+    assert data[2][4]['val'] == "10 new"
+    assert data[2][5]['val'] == '10'
+    assert data[3][0]['val'] == "11 mod"
+    assert data[3][1]['val'] == '12 mod'
+    assert data[3][2]['val'] == '13 new'
+    assert data[3][3]['val'] == '14'
+    assert data[3][4]['val'] == "15 new"
+    assert data[3][5]['val'] == '15'
+    assert data[4][0]['val'] == "16 mod"
+    assert data[4][1]['val'] == '17'
+    assert data[4][2]['val'] == '18 new'
+    assert data[4][3]['val'] == '19'
+    assert data[4][4]['val'] == "20 new"
+    assert data[4][5]['val'] == '20'
+
 
     delete_table(uuid, new_token_on_headers)
     data = get_table_data(uuid)
