713da9fd4c7caba71e879f4f7d403ba5a65a7b6f
1151186 <mouneshwara.sugappa@iqvia.com>
2023-04-05 22:07:11 +0530

    added alert_id column to table

--- app/models/pd_protocol_alert.py
+++ app/models/pd_protocol_alert.py
@@ -7,8 +7,8 @@ from sqlalchemy import Boolean, Column, Date, DateTime, String, Integer
 class ProtocolAlert(Base):
     __tablename__ = "pd_protocol_alert"
 
-    id = Column(Integer, primary_key=True)
-    aidocId = Column(String(100), primary_key=True)
+    id = Column(Integer)
+    aidocId = Column(String(100))
     protocol = Column(String(500))
     protocolTitle = Column(String(1500))
     readFlag = Column(Boolean(), default=False)
@@ -21,3 +21,4 @@ class ProtocolAlert(Base):
     timeCreated = Column(DateTime(timezone=True), default=datetime.datetime.utcnow)
     timeUpdated = Column(DateTime(timezone=True), default=datetime.datetime.utcnow)
     notification_delete = Column(Boolean(), default=False)
+    alert_id = Column(Integer, primary_key=True)
