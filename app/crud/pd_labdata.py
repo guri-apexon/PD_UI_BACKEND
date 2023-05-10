0610de089a7457aafb279eda1d2009bda809e98e
Gurpreet Singh <gurpreet.singh@iqvia.com>
2023-05-10 11:46:14 +0530

    Id fix

--- app/crud/pd_labdata.py
+++ app/crud/pd_labdata.py
@@ -72,10 +72,7 @@ class LabDataCrud(CRUDBase[IqvlabparameterrecordDb, LabDataCreate, LabDataUpdate
         try:
             for dt in arr_data:
                 column_id = dt.id
-                doc_id = dt.doc_id
-                roi_id = dt.roi_id
                 table_link_text = dt.table_link_text
-                table_roi_id = dt.table_roi_id
                 assessment = dt.assessment
                 procedure_panel = dt.procedure_panel
                 procedure_panel_text = dt.procedure_panel_text
@@ -86,9 +83,6 @@ class LabDataCrud(CRUDBase[IqvlabparameterrecordDb, LabDataCreate, LabDataUpdate
                 dts = '{:04d}{:02d}{:02d}{:02d}{:02d}{:02d}'.format(
                     date.year, date.month, date.day, date.hour, date.minute, date.second)
                 entity_rec = db.query(IqvlabparameterrecordDb).filter(
-                    IqvlabparameterrecordDb.doc_id == doc_id,
-                    IqvlabparameterrecordDb.table_roi_id == table_roi_id,
-                    IqvlabparameterrecordDb.roi_id == roi_id,
                     IqvlabparameterrecordDb.id == column_id
                 ).update({
                     'table_link_text': table_link_text,
