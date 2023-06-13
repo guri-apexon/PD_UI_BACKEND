7747c223b5760ee736feca8d30f942b4215d0fcc
U1149484 <mukeshkumar.kumawat@iqvia.com>
2023-06-13 20:32:55 +0530

    changes done for hierarchy

--- app/qc_ingest/model/documentpartlist_db.py
+++ app/qc_ingest/model/documentpartlist_db.py
@@ -74,7 +74,10 @@ class DocumentpartslistDb(SchemaBase):
             doc_id=prev_data.doc_id
             para_data.parent_id = doc_id
             update_partlist_index(session, DocumentpartslistDb.__tablename__, doc_id, para_data.link_id, para_data.sequence_id, CurdOp.CREATE) 
-        para_data.hierarchy = 'document'
+        if data.get('type') == 'table':
+            para_data.hierarchy = 'table'
+        else:
+            para_data.hierarchy = 'paragraph'
         para_data.group_type = 'IQVDocumentParts' 
         para_data.last_updated = get_utc_datetime()
         para_data.num_updates = 0
